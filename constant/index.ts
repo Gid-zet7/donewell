0879d8aa0f018a181a47efb513a0065961b68bf3
GideonAdonteng <adontenggideon57@gmail.com>
2024-04-14 16:56:52 -0700

    Added contants

---
+++ constant/index.ts
@@ -0,0 +1,26 @@
+export const Constants = [
+  {
+    src: "/classroom.png",
+    title: "Discipline",
+    key: 1,
+    text: "We commit to consistently break performance barriers without breaking the rules",
+  },
+  {
+    src: "/heart-red.svg",
+    title: "Passion",
+    key: 2,
+    text: "We commit to nuture an intense desire to go the extra mile for our customers and our teams",
+  },
+  {
+    src: "/patnership.png",
+    title: "Integrity",
+    key: 3,
+    text: "We commit to hold ourselves accountable to the highest standards of ethical business practice",
+  },
+  {
+    src: "/patners.png",
+    title: "Collaboration",
+    key: 4,
+    text: "We commit to work effectively together to achieve common goals",
+  },
+];
