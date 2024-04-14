8ce6c6c0a92d01d4ce272400e389030c2be195f2
GideonAdonteng <adontenggideon57@gmail.com>
2024-04-14 16:56:18 -0700

    Added components

---
+++ components/CoreValues.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Image from "next/image";
+import Paper from "@mui/material/Paper";
+import { Constants } from "@/constant";
+
+export default function CoreValues() {
+  return (
+    <>
+      <div className="flex flex-wrap justify-center text-center gap-1">
+        {Constants.map((constant) => (
+          <div key={constant.key}>
+            <div className="flex flex-col items-center justify-center">
+              <Image
+                src={constant.src}
+                alt="analysis"
+                width={50}
+                height={30}
+                className="items-center "
+              />
+              <p className=" mb-1 text-2xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-indigo-300">
+                {constant.title}
+              </p>
+              <div className="p-4 w-72">
+                <p className="mb-4 text-base font-normal dark:text-gray-400">
+                  {constant.text}
+                </p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,97 @@
+"use client";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+
+const Navbar = () => {
+  const [hamburgerBtn, setHamburgerBtn] = useState<HTMLElement | null>(null);
+  const [mobileMenu, setMobileMenu] = useState<HTMLElement | null>(null);
+  useEffect(() => {
+    const hamburgerBtn = document.getElementById("hamburger-button");
+    setHamburgerBtn(hamburgerBtn);
+    const mobileMenu = document.getElementById("mobile-menu");
+    setMobileMenu(mobileMenu);
+  }, []);
+
+  const toggleMenu = () => {
+    mobileMenu?.classList.toggle("hidden");
+    mobileMenu?.classList.toggle("flex");
+    hamburgerBtn?.classList.toggle("toggle-btn");
+  };
+
+  return (
+    <>
+      <header className="sticky top-0 z-10 bg-black">
+        <section>
+          <div className="flex justify-between py-5 px-8 border-b-4 gap-4 md:hidden border-red-600">
+            <h1 className="text-xl lg:text-3xl md:hidden text-white">
+              <Link href="/">Donewell Agent</Link>
+            </h1>
+            <button
+              id="hamburger-button"
+              className="relative h-8 w-8 cursor-pointer text-3xl md:hidden"
+              onClick={toggleMenu}
+            >
+              <div className="absolute top-4 -mt-0.5 h-0.5 w-5 rounded bg-white transition-all duration-500 before:absolute before:h-0.5 before:w-8 before:-translate-x-4 before:-translate-y-2 before:rounded before:bg-white dark:before:bg-white before:transition-all before:duration-500 before:content-[''] after:absolute after:h-0.5 after:w-8 after:-translate-x-4 after:translate-y-2 after:rounded after:bg-white dark:after:bg-white after:transition-all after:duration-500 after:content-['']"></div>
+            </button>
+          </div>
+          <nav
+            aria-label="main"
+            className="py-5 px-8 border-b-4 gap-4 sticky top-0 z-10 border-red-600 hidden space-x-8 text-xl md:block"
+          >
+            <div className="mx-auto flex max-w-7xl items-center justify-between text-white">
+              <h1 className="text-xl lg:text-3xl">
+                <Link href="/">Donewell Agent</Link>
+              </h1>
+
+              <ul className="flex gap-4 cursor-pointer">
+                <a href="/">
+                  <li>Home</li>
+                </a>
+
+                <a href="/products">
+                  <li>Products</li>
+                </a>
+                <li>Principles</li>
+                <li className="text-red-400">Dashboard</li>
+              </ul>
+            </div>
+          </nav>
+        </section>
+
+        <section
+          id="mobile-menu"
+          className="top-68 justify-center absolute hidden w-full origin-top animate-open-menu flex-col bg-black text-5xl"
+        >
+          <nav
+            className="flex min-h-screen flex-col items-center py-8 text-gray-300"
+            aria-label="mobile"
+          >
+            <a href="/" className="w-full py-6 text-center hover:opacity-90">
+              Home
+            </a>
+            <a
+              href="/products"
+              className="w-full py-6 text-center hover:opacity-90"
+            >
+              Products
+            </a>
+            <a
+              href="#principles"
+              className="w-full py-6 text-center hover:opacity-90"
+            >
+              Principles
+            </a>
+            <a
+              href="#dashboard"
+              className="w-full py-6 text-center hover:opacity-90"
+            >
+              Dashboard
+            </a>
+          </nav>
+        </section>
+      </header>
+    </>
+  );
+};
+
+export default Navbar;
